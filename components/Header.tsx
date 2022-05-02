18de7fce617951416348b077ecc9f66ebd862c74
Pranav <prnvbirajdar@gmail.com>
2022-05-02 00:36:43 -0400

    JS file test

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import Nav from '../components/Nav'
-import DataTable from './DataTable'
+import DataTableJS from './DataTableJS'
 
 export default function Header() {
   return (
@@ -11,7 +11,7 @@ export default function Header() {
       <main className="-mt-32">
         <div className="max-w-[1400px] mx-auto pb-12 sm:px-6 lg:px-8">
           <div className="bg-indigo-100 rounded-lg shadow px-5 py-6 sm:px-6 ">
-            <DataTable />
+            <DataTableJS />
           </div>
         </div>
       </main>
